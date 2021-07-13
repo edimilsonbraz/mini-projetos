ad0cc6c1216b5cfe3d7852150f3956f31b4f8197
Edimilson Braz <edimilsonbraz@pop-os.localdomain>
2021-07-13 08:55:59 -0300

    update images

--- 03 - slideshow/slideShow.js
+++ 03 - slideshow/slideShow.js
@@ -1,43 +1,92 @@
+// 'use strict';
+
+
+// const images = [
+//   {'id':'1', 'url':'./img/chrono.jpg'},
+//   {'id':'2', 'url':'./img/inuyasha.jpg'},
+//   {'id':'3', 'url':'./img/tenchi.jpg'},
+//   {'id':'4', 'url':'./img/tenjhotenge.jpg'},
+//   {'id':'5', 'url':'./img/yuyuhakusho.jpg'},
+//   {'id':'6', 'url':'./img/ippo.png'},
+
+// ]
+
+// const containerItems = document.querySelector('#container-items')
+
+// //Função que carrega as imagens
+// const loadImages = (images, containerItems) => {
+//   images.forEach(image => {
+//     containerItems.innerHTML += `
+//       <div class="item">
+//         <img src="${image.url}">
+//       </div>
+//     `
+//   })
+// }
+
+// loadImages(images, containerItems);
+
+// let items  = document.querySelectorAll('.item') // imagens dos slides atual
+
+// const previous = () => {
+//   containerItems.appendChild(items[0]) //AppendChild manda pro final da lista
+//   items = document.querySelectorAll('.item') // Atualiza a lista
+// }
+
+// const next = () => {
+//   const lastItem = items[items.length -1]
+//   containerItems.insertBefore(lastItem, items[0]) //insertBefore
+//   items  = document.querySelectorAll('.item') // imagens dos slides atual
+// }
+
+// document.querySelector('#previous').addEventListener('click', previous)
+// document.querySelector('#next').addEventListener('click', next)
+
+
+
+
 'use strict';
 
-
 const images = [
-  {'id':'1', 'url':'./img/chrono.jpg'},
-  {'id':'2', 'url':'./img/inuyasha.jpg'},
-  {'id':'3', 'url':'./img/tenchi.jpg'},
-  {'id':'4', 'url':'./img/tenjhotenge.jpg'},
-  {'id':'5', 'url':'./img/yuyuhakusho.jpg'},
-  {'id':'6', 'url':'./img/ippo.png'},
+  {'id':'1', 'url':'./img/launchbase.png'},
+  {'id':'2', 'url':'./img/dowhile.png'},
+  {'id':'3', 'url':'./img/rocket.png'},
+  {'id':'4', 'url':'./img/nave.jpg'},
+  {'id':'5', 'url':'./img/learning.png'},
+  {'id':'6', 'url':'./img/together.png'},
+];
 
-]
 
 const containerItems = document.querySelector('#container-items')
 
-//Função que carrega as imagens
 const loadImages = (images, containerItems) => {
   images.forEach(image => {
     containerItems.innerHTML += `
-      <div class="item">
-        <img src="${image.url}">
-      </div>
+    <div class="item">
+      <img src="${image.url}">
+    </div>
     `
   })
 }
 
-loadImages(images, containerItems);
+loadImages(images, containerItems)
 
-let items  = document.querySelectorAll('.item') // imagens dos slides atual
+let items = document.querySelectorAll('.item')
+
 
 const previous = () => {
-  containerItems.appendChild(items[0]) //AppendChild manda pro final da lista
-  items = document.querySelectorAll('.item') // Atualiza a lista
+  containerItems.appendChild(items[0])
+  items = document.querySelectorAll('.item')
 }
 
 const next = () => {
-  const lastItem = items[items.length -1]
-  containerItems.insertBefore(lastItem, items[0]) //insertBefore
-  items  = document.querySelectorAll('.item') // imagens dos slides atual
+  const lastItem = items[items.length - 1] ;
+  containerItems.insertBefore(lastItem, items[0])
+  items = document.querySelectorAll('.item')
+
 }
+
+
 
 document.querySelector('#previous').addEventListener('click', previous)
 document.querySelector('#next').addEventListener('click', next)
