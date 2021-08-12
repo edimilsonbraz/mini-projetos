5e43460dd361a74b4c66a3c7c91e6273c11e2aea
Edimilson Braz <edimilsonbraz@pop-os.localdomain>
2021-08-12 16:19:49 -0300

    Identificando, deletando e salvando dados no localStorage

--- 07-todo-list/app.js
+++ 07-todo-list/app.js
@@ -1,22 +1,27 @@
 'use strict';
 
 
-let banco = [
-{'tarefa': 'Estudar JS', 'status': ''},
-{'tarefa': 'Lavar Carro', 'status': 'checked'},
-{'tarefa': 'Assistir Filme', 'status': 'checked'},
+// let banco = [
+// {'tarefa': 'Estudar JS', 'status': ''},
+// {'tarefa': 'Lavar Carro', 'status': 'checked'},
+// {'tarefa': 'Assistir Filme', 'status': 'checked'},
 
-];
+// ];
+
+
+//Armazenando no LocalStorage
+const getBanco = () => JSON.parse(localStorage.getItem('todoList')) ?? [];
+const setBanco = (banco) => localStorage.setItem('todoList', JSON.stringify(banco))
 
 //Cria estrutura do item na tela
-const criarItem = (tarefa, status='') => {
+const criarItem = (tarefa, status, indice) => {
   const item = document.createElement('label')
   item.classList.add('todo__item')
   item.innerHTML = 
   `
-    <input type="checkbox" ${status}>
+    <input type="checkbox" ${status} data-indice=${indice}>
     <div>${tarefa}</div>
-    <input type="button" value="X">
+    <input type="button" value="X" data-indice=${indice}>
   `
   document.getElementById('todoList').appendChild(item)
 }
@@ -32,23 +37,55 @@ const limparTarefas = () => {
 
 const atualizarTela = () => {
   limparTarefas()
-  banco.forEach(item => criarItem(item.tarefa, item.status));
+  const banco = getBanco();
+  banco.forEach((item, indice) => criarItem(item.tarefa, item.status, indice));
 }
 
 
+//QUANDO ALGUEM DIGITAR E CLICAR ENTER SERÁ INSERIDO UMA TAREFA
 const inserirItem = (evento) => {
   const tecla = evento.key;
   const texto = evento.target.value;
   if(tecla === 'Enter') {
+    const banco = getBanco();
     banco.push({'tarefa': texto, 'status': ''})
+    setBanco(banco) //envia para LocalStorage
     atualizarTela();
-    evento.target.value =""; // limpa o input
+    evento.target.value =""; // limpa o input(tarefa)
   }
 }
 
 
-//QUANDO ALGUM DIGITAR E CLICAR ENTER SERÁ INSERIDO UMA TAREFA
+//Função que remove tarefas
+const removerItem = (indice) => {
+  const banco = getBanco();
+  banco.splice(indice, 1);
+  setBanco(banco)
+  atualizarTela();
+}
+
+const atualizarItem = (indice) => {
+  const banco = getBanco();
+  banco[indice].status = banco[indice].status === '' ? 'checked' : '';
+  setBanco(banco) //envia para LocalStorage
+  atualizarTela()
+}
+
+//Ver qual elemento foi clicado
+const clickItem = (evento) => {
+  const elemento = evento.target;
+  if(elemento.type === 'button') {
+    const indice = elemento.dataset.indice;
+    removerItem(indice);
+  }else if(elemento.type === 'checkbox') {
+    const indice = elemento.dataset.indice
+    atualizarItem(indice)
+  }
+}
+
 document.getElementById('newItem').addEventListener('keypress', inserirItem)
+document.getElementById('todoList').addEventListener('click', clickItem)
 
 
+//Executando a funçãp atualizarTela
 atualizarTela()
